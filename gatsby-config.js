0f9d615f8b7177de28bb2c8c035400d06a4e63cc
Theo <theodorechen03@gmail.com>
2024-02-01 16:19:07 -0500

    Update 1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,76 +1,110 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://theoc.dev`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Theo Chen',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Theo Chen | Computer Science & Japanese @ BU`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `3rd-Year at Boston University studying Computer Science and Japanese. President of the Boston University Japanese Student Association`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@theoc3`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/theoc3`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/theo-chen-08bb12224/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `I'm a 3rd-year at Boston University.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'Japanese Handwriting Rater',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'A script that rates Japanese kana handwriting using EasyOCR library and a custom machine learning model trained on generated handwriting images.',
+        link: 'https://github.com/theoc3/handwriting-rating',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Kyoudai Matching Program',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
+          'A program that extracts csv files with a Python script to generate an input for a C++ program that uses a bipartite graph and a max-cost min-flow algorithm to match “bigs” and “littles" for the BUJSA Kyoudai program.',
+        link: 'https://github.com/schuyler1007/bujsa',
       },
       {
-        name: 'Another Cool Project',
+        name: 'Stationery Blog',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'A blog created with the Next.js framework displaying blog posts and reviews of stationery written in Markdown.',
+        link: 'https://theosbungu.com',
       },
+      {
+        name: 'Spark Bytes MVP Demo Website',
+        description:
+          'A MVP website hosting user generated posts for food leftovers at BU events with user authentication via OAuth 2.0 and Prisma interactions with a PostgreSQL database using Express.js API access. Worked in a simulated scrum team of 4, with 3 sprints total.',
+        link: 'https://spark-bytes-project-team-5-demo.vercel.app/'
+      },
+      {
+        name: 'Space News Website',
+        description:
+          'A website that displays the latest news about space using React and a public Space News API.',
+        link: 'https://cs392-space-news-project.vercel.app/'
+      },
+      {
+        name: 'Japanese Study Program',
+        description:
+          'A Python program for memorizing the Japanese writing systems of Hiragana and Katakana that adjusts its difficulty based on the user’s familiarity with each character as they answer correctly or incorrectly, keeping track of response time and accuracy.',
+        link: 'https://github.com/theoc3/JapanesePractice'
+      },
+      {
+        name: 'Rig the Dice',
+        description:
+          'A 3D Unity game developed in 48 hours for the annual GMTK Game Jam. The theme of “Roll of the Dice” is interpreted as rigging dice so that they have the desired outcome for a basic gambling game.',
+        link: 'https://talcgames.itch.io/rig-the-dice'
+      },
+      {
+        name: 'Color Theory',
+        description:
+          'A 2D Unity game developed in 48 hours for the annual GMTK Game Jam. The theme of “Joined Together” is interpreted as both joining together primary colors to create secondary colors and combining the player with same colored enemies to score.',
+        link: 'https://talcgames.itch.io/color-theory'
+      }
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Boston University Japanese Student Association',
+        description: 'President: June 2023 - Present | Secretary: June 2022 - Present | Underclassmen Representative: January 2022 - May 2022',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Timewitch Inc.',
+        description: 'Full-Stack Developer Intern, December 2023 - January 2024',
+        link: 'https://timewitch.jp/',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Cardinal Blue Software, Inc.',
+        description: 'Product Operations Intern, June 2017 - July 2017',
+        link: 'https://picc.co/',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'Programming Languages',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Python, C, C#, C++, Java, Git, Assembly, Bash, HTML, CSS, Javascript, Typescript, SQL',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        name: 'Web Development',
+        description: 'React, Node.js, Next.js, Express.js, Prisma, Redis, Zod, Tailwind, OAuth 2.0, PostgreSQL, Vercel, Railway, Azure',
       },
       {
-        name: 'Other',
+        name: 'Machine Learning/Data Science',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'NumPy, pandas, scikit-learn, PyTorch, TensorFlow, Matplotlib, EasyOCR',
       },
+      {
+        name: 'Software/Tools',
+        description: 
+          'Unity 2D/3D, Vim, Emacs, Docker, VSCode, Spyder, Adobe Suite, Microsoft Suite, Google Suite, Logic Pro, Blender',
+      }
     ],
   },
   plugins: [
