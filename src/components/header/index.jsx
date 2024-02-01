90844d1e705023e94f5e7bfaa8a8c17b3558431b
Theo <theodorechen03@gmail.com>
2024-02-01 16:43:24 -0500

    Removed blog button

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -59,13 +59,6 @@ const Header = ({ metadata = {}, noBlog = false }) => {
               </a>
             </li>
           )}
-          {!noBlog && (
-            <li className={classes.item}>
-              <Link className={classes.link} to="/blog">
-                Blog
-              </Link>
-            </li>
-          )}
         </ul>
       </div>
     </div>
