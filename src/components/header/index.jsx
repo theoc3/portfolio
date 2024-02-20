c4ebb6d2170612c8b40a31e0b07fc93dd6d8ebff
Theo <theodorechen03@gmail.com>
2024-02-20 16:26:46 -0500

    Header links now also open in new tab

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -47,14 +47,14 @@ const Header = ({ metadata = {}, noBlog = false }) => {
           )}
           {github && (
             <li className={classes.item}>
-              <a className={classes.link} href={github}>
+              <a className={classes.link} href={github} target="_blank" rel="noopener noreferrer">
                 GitHub
               </a>
             </li>
           )}
           {linkedin && (
             <li className={classes.item}>
-              <a className={classes.link} href={linkedin}>
+              <a className={classes.link} href={linkedin} target="_blank" rel="noopener noreferrer">
                 LinkedIn
               </a>
             </li>
