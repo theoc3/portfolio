ce13835ac5912f73530ba4110697a5a896731db6
Theo <theodorechen03@gmail.com>
2024-03-14 01:03:45 -0400

    Tech stack

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -5,10 +5,11 @@ const classes = {
   wrapper: 'mb-6',
   name: 'font-semibold text-gray-900 pb-1',
   description: 'text-md text-gray-600 font-light',
-  arrow: 'inline-block transform transition-transform duration-200', // Add a class for the arrow
+  arrow: 'inline-block transform transition-transform duration-200', 
+  tech: 'inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2', 
 };
 
-const SummaryItem = ({ name, description, link = false, internal = false }) => {
+const SummaryItem = ({ name, description, link = false, internal = false, techStack = '' }) => {
   let linkContent;
   if (internal) {
     linkContent = (
@@ -24,6 +25,10 @@ const SummaryItem = ({ name, description, link = false, internal = false }) => {
     );
   }
 
+  const techStackItems = techStack.split(',').map((tech, index) => (
+    <span key={index} className={classes.tech}>{tech.trim()}</span>
+  ));
+
   return (
     <div className={classes.wrapper}>
       <h3
@@ -33,6 +38,7 @@ const SummaryItem = ({ name, description, link = false, internal = false }) => {
       >
         {link ? linkContent : name}
       </h3>
+      <div>{techStackItems}</div>
       <p className={classes.description}>{description}</p>
     </div>
   );
