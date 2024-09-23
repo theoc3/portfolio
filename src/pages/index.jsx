eb42e9d0e26fa2087a8d138675008cf72122a3df
Theo <theodorechen03@gmail.com>
2024-09-23 20:24:05 +0900

    fix activities

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ import Layout from '../components/layout';
 import SectionAbout from '../components/section-about';
 import SectionBlog from '../components/section-blog';
 import SectionExperience from '../components/section-experience';
+import SectionActivities from '../components/section-activities';
 import SectionProjects from '../components/section-projects';
 import SectionSkills from '../components/section-skills';
 import SEO from '../components/seo';
