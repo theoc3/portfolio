332e4469b5e4c137350b4fa2015fcd90956f57ad
Theo <theodorechen03@gmail.com>
2025-01-13 21:57:05 -0800

    fix line break

--- src/components/section-about/index.jsx
+++ src/components/section-about/index.jsx
@@ -6,7 +6,9 @@ const SectionAbout = ({ about }) => {
   return (
     <Section title="About Me">
       <div className="mb-6">
-        <p>{about}</p>
+        {about.split('<br/>').map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
       </div>
     </Section>
   );
