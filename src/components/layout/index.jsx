fa6e233dfb145e9bef8ea5fff147e6d7bb5e0fde
Theo <theodorechen03@gmail.com>
2025-12-03 09:56:07 -0500

    Move background up

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -4,13 +4,13 @@ const classes = {
   wrapper: 'p-8 relative w-full max-w-screen-2xl mx-auto md:p-12',
   outerWrapper: 'relative',
   watermark:
-    'hidden fixed transform right-0 top-0 z-0 xl:block text-gray-100 font-bold select-none pointer-events-none',
+    'hidden fixed transform right-0 z-0 xl:block text-gray-100 font-bold select-none pointer-events-none',
 };
 
 const Layout = ({ children }) => {
   return (
     <div className={classes.outerWrapper}>
-      <div className={classes.watermark} style={{ fontSize: '20rem' }}>
+      <div className={classes.watermark} style={{ fontSize: '20rem', top: '-3rem' }}>
         テオ
       </div>
       <div className={classes.wrapper}>{children}</div>
